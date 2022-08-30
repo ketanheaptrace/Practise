4e857e88b45d5eea5c1f0d5d58c4f32b14833834
ketanheaptrace <95910969+ketanheaptrace@users.noreply.github.com>
2022-08-30 10:50:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Home from "./Home";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <Home />
+    </div>
+  );
+}
